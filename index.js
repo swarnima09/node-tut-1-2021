0087a55cb1f93df6a57397023702fe501b4d0b2b
Swarnima  Gupta <swarnima.gupta@symbtechnologies.com>
2021-11-14 12:15:00 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const app = require('./app')
+
+const fs = require('fs');
+
+// const http = require('http');
+
+// http.createServer((req,resp) => {
+//     resp.write("Hello I am Swarnima");
+//     resp.end()
+// }).listen(4500)
+console.log("hi")
