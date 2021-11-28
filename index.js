2ec6611b04189c7d13446f1ba76de652cb2116f0
Swarnima Gupta <sg09924@gmail.com>
2021-11-28 22:44:50 +0530

    complted crud

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
-const dbConnect = require('./mongodb');
+const mongoose = require('mongoose');
 
-// lines to read data
-const main   = async () => {
-    let data = await dbConnect;
-    data = await data.find().toArray();
-    console.warn(data)
+mongoose.connect("mongodb://localhost:27017/e-comm");
 
+const ProductSchema = new mongoose.Schema({
+    name : String,
+    brand : String,
+    price : Number
+});
+
+const saveInDB =  async () => {
+     const ProductModel = mongoose.model('product',ProductSchema)
+    let data = new ProductModel({name:"Apple",host:"Apple",brand: "vivo",price:23000});
+    let result =  await data.save();
+    console.log(result);
 }
 
-main();
+const updateInDB =  async () => {
+    const ProductModel = mongoose.model('product',ProductSchema)
+    let data = await ProductModel.updateMany({name:"sakshii"},{$set:{price: 4000}})
+    console.log(result);
+}
+
+const deleteInDB =  async () => {
+    const ProductModel = mongoose.model('product',ProductSchema)
+    let data = await ProductModel.deleteMany({name:"sakshii"});
+}
+
+deleteInDB()
+
+
